2ea16a7da9d244143ece8e426648bb07a8b2c2ab
Karen Payne <paynekaren@comcast.net>
2022-09-08 07:09:57 -0700

    new extensions and samples

--- TimeClassLibrary/Extensions.cs
+++ TimeClassLibrary/Extensions.cs
@@ -5,23 +5,46 @@ public static class Extensions
     /// <summary>
     /// Is end time prior to start time
     /// </summary>
-    public static bool IsValidEndTime(this TimeOnly endTimeOnly, TimeOnly starTimeOnly) => endTimeOnly.Hour < starTimeOnly.Hour;
+    public static bool IsValidEndTime(this TimeOnly endTime, TimeOnly startTime) => endTime.Hour < startTime.Hour;
+
+
     /// <summary>
     /// Is start time after end time
     /// </summary>
-    public static bool IsValidStartTime(this TimeOnly starTimeOnly, TimeOnly endTimeOnly)
-        => endTimeOnly.Hour > starTimeOnly.Hour;
+    public static bool IsValidStartTime(this TimeOnly startTime, TimeOnly endTime)
+        => endTime.Hour > startTime.Hour;
+
 
     /// <summary>
     /// Determine if TimeOnly is before another TimeOnly
     /// </summary>
-    public static bool IsLessThan(this TimeOnly endTimeOnly, TimeOnly starTimeOnly) => endTimeOnly.CompareTo(starTimeOnly) < 0;
+    public static bool IsLessThan(this TimeOnly endTimeOnly, TimeOnly startTime) => endTimeOnly.CompareTo(startTime) < 0;
+
+
+    public static bool IsLessThan<T>(this T sender, T compareTo) where T : IComparable<T> => compareTo.CompareTo(sender) < 0;
+
     /// <summary>
     /// Determine if TimeOnly is after another TimeOnly
     /// </summary>
-    public static bool IsGreaterThan(this TimeOnly endTimeOnly, TimeOnly starTimeOnly) => endTimeOnly.CompareTo(starTimeOnly) > 0;
+    public static bool IsGreaterThan(this TimeOnly endTime, TimeOnly startTime) => endTime.CompareTo(startTime) > 0;
+
+
+    public static bool IsGreaterThan<T>(this T sender, T compareTo) where T : IComparable => sender.CompareTo(compareTo) > 0;
+
+
     /// <summary>
     /// Determine if TimeOnly is the same as another TimeOnly
     /// </summary>
-    public static bool AreSame(this TimeOnly endTimeOnly, TimeOnly starTimeOnly) => endTimeOnly.CompareTo(starTimeOnly) == 0;
+    public static bool AreSame(this TimeOnly endTimeOnly, TimeOnly startTimeOnly) => endTimeOnly.CompareTo(startTimeOnly) == 0;
+
+
+    /// <summary>
+    /// Get time between two TimeOnly as a TimeSpan
+    /// </summary>
+    /// <remarks>
+    /// Sure this code can be circumvented but using an extension the name provides clarity
+    /// </remarks>
+    public static TimeSpan Duration(this TimeOnly endTimeOnly, TimeOnly starTimeOnly) => endTimeOnly - starTimeOnly;
+
+
 }
--- TimeHelpersConsoleApp/Program.cs
+++ TimeHelpersConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using TimeClassLibrary;
+﻿using System.Linq;
+using TimeClassLibrary;
 using TimeHelpersConsoleApp.Classes;
 
 namespace TimeHelpersConsoleApp
@@ -8,12 +9,31 @@ namespace TimeHelpersConsoleApp
         static void Main(string[] args)
         {
 
-            Console.WriteLine();
-
-            GreaterThan_LesserThan();
+            //TimeOnly timeOnly1 = new(14, 0, 0);
+            //TimeOnly timeOnly2 = new(13, 0, 0);
+            //Console.WriteLine(timeOnly1.IsLessThan(timeOnly2));
+            //Console.WriteLine(timeOnly2.IsLessThan(timeOnly1));
+            //Duration();
+            //GreaterThan_LesserThan();
             //IsBetween();
 
             Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Example to get duration between a end and start TimeOnly
+        /// </summary>
+        private static void Duration()
+        {
+            TimeOnly endTime = new(14, 0, 0);
+            TimeOnly startTime = new(7, 0, 0);
+
+
+            TimeSpan duration = DateTime.Parse(endTime.ToString()).Subtract(DateTime.Parse(startTime.ToString()));
+
+            Console.WriteLine(duration.ToString(@"hh\:mm"));
+
+            Console.WriteLine(endTime.Duration(startTime).ToString(@"hh\:mm"));
         }
 
         /// <summary>
@@ -52,6 +72,10 @@ namespace TimeHelpersConsoleApp
 
             AnsiConsole.MarkupLine($"[white]timeOnly2.IsValidEndTime(timeOnly1)[/] {timeOnly2.IsValidEndTime(timeOnly1).ToYesNoString()}");
             AnsiConsole.MarkupLine($"[white]    timeOnly2.IsLessThan(timeOnly1)[/] {timeOnly2.IsLessThan(timeOnly1).ToYesNoString()}");
+            AnsiConsole.MarkupLine($"[white]    timeOnly2.IsLessThan(timeOnly1)[/] {timeOnly2.IsLessThan(timeOnly1).ToYesNoString()}");
+
+
+
             AnsiConsole.MarkupLine($"[white] timeOnly2.IsGreaterThan(timeOnly1)[/] {timeOnly2.IsGreaterThan(timeOnly1).ToYesNoString()}");
 
 
