30ae1dc955714d95ba9d626200c935e7eb6883d9
Karen Payne <paynekaren@comcast.net>
2022-11-11 09:04:37 -0800

    dotnet core 7 project added

---
+++ TimeOnlyCore7App/Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace TimeOnlyCore7App
+{
+    internal partial class Program
+    {
+        [ModuleInitializer]
+        public static void Init()
+        {
+            Console.Title = "Code sample: .NET Core 7 - TimeOnly";
+            WindowUtility.SetConsoleWindowPosition(WindowUtility.AnchorWindow.Center);
+        }
+    }
+}
---
+++ TimeOnlyCore7App/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TimeOnlyCore7App
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TimeOnly timeOnly1 = new TimeOnly();
+            AnsiConsole.MarkupLine($"12 AM [cyan]{timeOnly1}[/]");
+
+            Console.WriteLine();
+
+            TimeOnly timeOnly2 = new TimeOnly(13,15,10,20,30);
+            AnsiConsole.MarkupLine($"{timeOnly2.ToShortTimeString()}");
+            AnsiConsole.MarkupLine($"{timeOnly2.ToLongTimeString()}");
+            AnsiConsole.MarkupLine($"{timeOnly2.ToString(@"hh\:mm\:ss\.ff")}");
+
+            Console.WriteLine();
+
+            var ts = timeOnly2.ToTimeSpan();
+
+            AnsiConsole.MarkupLine($"As [b]TimeSpan[/] [cyan]{ts.ToString()}[/]");
+            AnsiConsole.MarkupLine($"As [b]TimeSpan[/] [cyan]{ts:g}[/]");
+            Console.ReadLine();
+        }
+    }
+}
